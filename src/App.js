da487ce48184fa405283024468d4fcdf2e7bf918
Bartlomiej Witczak <witczakbartlomiej@gmail.com>
2017-07-10 10:29:11 +0200

    Removed expenses list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from 'react'
 import TopBar from './top-bar'
-import ExpensesList from './expenses-list'
-import { expenses } from './data'
 
 class App extends React.Component {
-	constructor(props) {
-		super(props)
-
-		this.state = {
-			expenses
-		}
-	}
 
 	render() {
-		const { expenses } = this.state
 		return (
 			<div>
 				<TopBar/>
@@ -21,7 +11,6 @@ class App extends React.Component {
 				<div className='container-fluid'>
 					<div className='row fill-height'>
 						<div className='col-md-8 pb-3'>
-							<ExpensesList expenses={expenses}/>
 						</div>
 						<div className='col-md-4'>
 						</div>
