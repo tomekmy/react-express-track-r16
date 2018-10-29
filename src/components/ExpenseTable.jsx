1d005bb23c3c28708ff2afa7edf131c0fdff7f2c
Myśliwiec Tomasz (FIRMA ZEWNĘTRZNA ) <tomysliwiec@pzu.pl>
2018-10-29 12:55:35 +0100

    Remove console.log

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
 const ExpenseTable = (props) => {
-  console.log(props);
-
   const generateTable = () => {
     return props.expenses.map((element) => {
       return (
-        <div className="expenseTable">
+        <div key={element.id} className="expenseTable">
           <div>Category</div><div>{element.category}</div>
           <div>Date</div><div>{element.date}</div>
           <div>Amount</div><div>{element.amount}</div>
--- src/components/expense.jsx
+++ src/components/expense.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Expense = (props) => {
-  console.log(props);
   return (
     <div className="expenseTable">
       <div>Category:</div><div>{props.expenses[0].category}</div>
