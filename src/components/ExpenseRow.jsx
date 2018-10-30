15c0014ec51d921d8535eaa0dfba3339fcff052c
Myśliwiec Tomasz (FIRMA ZEWNĘTRZNA ) <tomysliwiec@pzu.pl>
2018-10-30 09:25:30 +0100

    Add propTypes

---
+++ src/components/ExpenseRow.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const ExpenseRow = props => {
+	const data = {...props.expense};
+	return (
+		<tr onClick={() => props.onSelect(data)} className={props.selected ? 'selected' : null}>
+			<td>{data.category}</td>
+			<td>{data.date}</td>
+			<td>{data.amount + data.currency}</td>
+			<td>{data.description}</td>
+		</tr>
+	);
+};
+
+ExpenseRow.propTypes = {
+  expense: PropTypes.shape({
+    category: PropTypes.string,
+		date: PropTypes.string,
+		amount: PropTypes.number,
+		currency: PropTypes.string,
+		description: PropTypes.string
+  })
+};
+
+export default ExpenseRow
--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,20 +1,60 @@
 import React from 'react'
+import ExpenseRow from './ExpenseRow'
 
-const ExpenseTable = (props) => {
-  const generateTable = () => {
-    return props.expenses.map((element) => {
-      return (
-        <div key={element.id} className="expenseTable">
-          <div>Category</div><div>{element.category}</div>
-          <div>Date</div><div>{element.date}</div>
-          <div>Amount</div><div>{element.amount}</div>
-          <div>Description</div><div>{element.description}</div>
-        </div>
-      );
-    });
-  };
+class ExpenseTable extends React.Component {
 
-  return generateTable()
-};
+	state = {
+		selectedID: ''
+	}
+
+	selectElement = ({id}) => {
+		this.setState({
+			selectedID: id
+		});
+	}
+
+	generateTable = (props) => {
+		return props.map(element => {
+			return (
+					<ExpenseRow
+						expense={element}
+						key={element.id}
+						onSelect={this.selectElement}
+						selected={this.state.selectedID === element.id}
+					/>
+			);
+		});
+	};
+
+	render() {
+		return (
+			<table className="expenseTable">
+				<tbody>
+					<tr>
+						<td>
+							<strong>Category</strong>
+						</td>
+						<td>
+							<strong>Date</strong>
+						</td>
+						<td>
+							<strong>Amount</strong>
+						</td>
+						<td>
+							<strong>Description</strong>
+						</td>
+					</tr>
+					
+					{ this.generateTable(this.props.expenses) }
+					<tr>
+						<td></td>
+						<td></td>
+						<td>{this.props.expenses.reduce((sum, element) => sum + element.amount, 0)}$</td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>);
+	}
+}
 
 export default ExpenseTable
