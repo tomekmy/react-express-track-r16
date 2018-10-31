b556213cbcf43bf1c90217afeb16e7da0a3fbfe5
Myśliwiec Tomasz (FIRMA ZEWNĘTRZNA ) <tomysliwiec@pzu.pl>
2018-10-31 12:06:13 +0100

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import TopBar from './top-bar'
-import Expense from './components/Expense'
-import ExpenseTable from './components/ExpenseTable'
-import NewExpense from './components/NewExpense'
-import WithLogging from './components/WithLoading'
-import {keyBy, values} from 'lodash'
+import React from 'react';
 
-// import { expenses as data } from './data'
+import TopBar from './top-bar';
+import Expense from './components/Expense';
+import ExpenseTable from './components/ExpenseTable';
+import NewExpense from './components/NewExpense';
+import WithLogging from './components/WithLoading';
+import { connect } from 'react-redux';
+
+import { values } from 'lodash';
+
+// import { expenses as data } from './data';
+
+const mapStateToProps = state => {
+	return {
+		expenses: state.expenses
+	}
+}
 
 class App extends React.Component {
 
@@ -14,26 +23,27 @@ class App extends React.Component {
 		super(props);
 
 		this.state = {
-			expenses: [],
 			selectedID: ''
 		}
 	}
 
-	componentDidMount() {
-		fetch('http://192.168.1.118:8080/data')
-			.then((response) => {
-				return response.json();
-			})
-			.then((myJson) => {
-				this.setState({expenses: keyBy(myJson, 'id')});
-		});
-	}
+	// componentDidMount() {
+	// 	fetch('http://192.168.1.118:8080/data')
+	// 		.then((response) => {
+	// 			return response.json();
+	// 		})
+	// 		.then((myJson) => {
+	// 			this.setState({expenses: keyBy(myJson, 'id')});
+	// 	});
+	// }
 
 	markAsPaid = (id, value) => {
-		const expenses = this.state.expenses;
-		const expensesCopy = {...expenses, [id]:{...expenses[id], paid: value}}
-		this.setState({
-			expenses: expensesCopy
+		this.props.dispatch({
+			type: 'MARK_AS_PAID',
+			payload: {
+				id,
+				value
+			}
 		});
 	}
 
@@ -50,7 +60,7 @@ class App extends React.Component {
 
 
 	render() {
-		const expense = <Expense expenses={this.state.expenses[this.state.selectedID]} />;
+		const expense = <Expense expenses={this.props.expenses[this.state.selectedID]} />;
 
 		return (
 			<div className="wrapper">
@@ -58,7 +68,7 @@ class App extends React.Component {
 				{this.state.selectedID ? expense : null}
 				<ExpenseTable
 					markAsPaid={this.markAsPaid}
-					expenses={values(this.state.expenses)}
+					expenses={values(this.props.expenses)}
 					selectedID={this.state.selectedID}
 					selectElement={this.selectElement} />
 
@@ -74,4 +84,4 @@ class App extends React.Component {
 	}
 }
 
-export default WithLogging(App)
+export default connect(mapStateToProps)(WithLogging(App))
--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.scss'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { compose, createStore, combineReducers, applyMiddleware } from 'redux';
+import { keyBy } from 'lodash';
+import './index.scss';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+import { expenses as data } from './data';
+
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+// const store = createStore(
+//   combineReducers({
+//     expenses: state => state || {}
+//   }),
+//   baseState,
+//   composeEnhancers(applyMiddleware(thunk))
+// )
+
+const app = combineReducers(
+  {
+    expenses: (state={}, action) => {
+    const { payload, type } = action;
+
+    switch(type) {
+      case 'MARK_AS_PAID':
+        return {...state, [payload.id]:{...state[payload.id], paid: payload.value}}
+      default:
+        return state
+    }
+  }
+});
+
+const initialState = {
+  expenses: keyBy(data, 'id')
+}
+const store = createStore(app, initialState, composeEnhancers());
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
+    registerServiceWorker()
